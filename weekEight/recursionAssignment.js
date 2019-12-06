7e552095c302194aa42cb10eae0a1f7316bbcbdb
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2019-12-05 19:16:13 -0500

    homework for Recursion Class

---
+++ weekEight/recursionAssignment.js
@@ -0,0 +1,16 @@
+// Closed Form
+
+const closedForm = (k) => {
+    let answer = 0;
+    for(let i=1; i<=k; i++){
+        answer += i;
+    }
+    return answer;
+}
+
+const recursiveForm = (k) => {
+    if(k == 1){return 1}
+    return k + recursiveForm(k-1)
+}
+
+console.log(recursiveForm(5));
